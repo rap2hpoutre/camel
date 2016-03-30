5e3f1ba179c165699de329ce90d9de1b2035a5b5
raphael <raphael@ornikar.com>
2016-03-30 16:21:41 +0200

    haha

---
+++ Assets/gates/GateController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class GateController : MonoBehaviour {
+
+	public string scene;
+
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		if (col.gameObject.name == "camel")
+		{
+			SceneManager.LoadScene (scene);
+		}
+	}
+}
---
+++ Assets/grunts/OneAxisGruntController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OneAxisGruntController : GruntController {
+
+	public float speed = 0.02f;
+	public int direction = 1;
+	public bool vertical = true;
+
+	void FixedUpdate() 
+	{
+		if (vertical) {
+			transform.Translate (0, direction * speed, 0, Space.World);
+		} else {
+			transform.Translate (direction * speed, 0, 0, Space.World);
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D col)
+	{
+		if (col.gameObject.tag != "grunt")
+		{
+			direction *= -1;
+		}
+	}
+}
